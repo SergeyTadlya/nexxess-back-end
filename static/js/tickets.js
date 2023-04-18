923c4dfcd0f6468d75b78d8c89a7d9f9b9d46468
ngwr21 <igoronik11@gmail.com>
2023-04-19 00:09:15 +0300

    add some front changes

--- static/js/tickets.js
+++ static/js/tickets.js
@@ -92,16 +92,25 @@ document.addEventListener('keydown', (e) => {
   }
 })
 
-//__________BURGER__________
+//__________BURGER AND FILTER__________
 
 const asideInfo = document.querySelector('.aside-info')
 const burger = document.querySelector('.burger')
+const asidefilter = document.querySelector('.aside-filter')
+const filterBtn = document.querySelector('.content__filter-btn')
 
+//____BURGER___
 burger.addEventListener('click', menu)
 
 function menu() {
+  if (burger.classList.contains('burger-filter--active')) {
+    burger.classList.remove('burger-filter--active')
+    asidefilter.classList.remove('aside-filter--active')
+    document.body.classList.remove('body--active')
+    return
+  }
+
   burger.classList.toggle('burger--active')
-  asidefilter.classList.remove('aside-filter--active')
   asideInfo.classList.toggle('aside-info--active')
   
   if(document.body.classList.contains('body--active') && !burger.classList.contains('burger--active')) {
@@ -111,16 +120,13 @@ function menu() {
   }
 }
 
-//__________FILTER_________
+//_____FILTER____
 
-const asidefilter = document.querySelector('.aside-filter')
-const filterBtn = document.querySelector('.content__filter-btn')
 filterBtn.addEventListener('click', filters)
 
 function filters() {
-  // burger.classList.toggle('burger--active')
-  asideInfo.classList.remove('aside-info--active')
-  asidefilter.classList.toggle('aside-filter--active')
+  burger.classList.add('burger-filter--active')
+  asidefilter.classList.add('aside-filter--active')
 
   if (document.body.classList.contains('body--active')) {
     document.body.classList.remove('body--active')
@@ -130,7 +136,7 @@ function filters() {
 }
 
 
-//_______Mobile content arrow_____
+//__________Mobile content arrow_____
 
 const contentBox = document.querySelectorAll('.content__body-box')
 const contentBtn = document.querySelectorAll('.content__body-btn')
