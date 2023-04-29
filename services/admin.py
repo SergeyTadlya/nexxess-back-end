c63a8772a1c9c409e888b37e1b99037331d3c29c
ubuntu server <i.onofriichuk@avivi.com.ua>
2023-04-29 22:57:48 +0000

    addabout

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Service
 
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'currency', 'created_at', 'updated_at')
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ('title', 'price', 'currency')
 # Register your models here.
